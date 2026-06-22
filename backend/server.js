d5d449492a63337379c0fab865e2ead69d4d8acd
dhanishpraveen <dhanishpraveen0510@gmail.com>
2026-06-22 14:57:24 +0530

    cors implemented

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,13 @@
 import express from 'express'
 import "dotenv/config"
+import cors from "cors"
 import { createTransactionTable, sql } from './config/db.js'
 import transactionRoute from './routes/transactionRoutes.js'
 
 const app = express()
 const PORT = process.env.PORT
 
-
+app.use(cors())
 app.use(express.json())
 app.use('/api/transactions',transactionRoute)
 
